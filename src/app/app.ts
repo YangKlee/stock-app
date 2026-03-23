a8ae5dce3029efc4d3b7b76b6effe9956814b9ee
YangKlee <147032564+YangKlee@users.noreply.github.com>
2026-03-23 16:19:28 +0700

    fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,12 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import { Footer } from './layout/footer/footer';
 import { StockList } from "./stock/stock-list/stock-list";
-import { CreateStock } from './stock/create-stock/create-stock';
 import {CreateStockReactform} from "./stock/create-stock-reactform/create-stock-reactform"
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, Footer, StockList, CreateStock, CreateStockReactform],
+  imports: [RouterOutlet, Header, Footer, StockList, CreateStockReactform],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
