ced09c58ec04ea33d7c35337be844f1a1e1b4c57
YangKlee <147032564+YangKlee@users.noreply.github.com>
2026-04-06 16:54:46 +0700

    fixxxx

---
+++ src/app/layout/main-layout/main-layout.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Footer } from '../footer/footer';
+import { Header } from '../header/header';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [Footer, Header, RouterOutlet],
+  templateUrl: './main-layout.html',
+  styleUrl: './main-layout.css',
+})
+export class MainLayout {
+
+}
